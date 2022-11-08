0155ad2dc421adbfea5a8cb697bc4da2501fa266
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-11-08 13:33:48 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -4,3 +4,12 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = [  "@","%","+","\\","/","'","!","#","$","^","?",":",",",")","(","}","{","]","[","~","-","_",".",];
 
 var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  
+  generateBtn.addEventListener("click", writePassword);
+  
