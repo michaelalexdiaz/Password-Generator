37335477319ec0208f166409c1368e2971328050
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-11-08 13:24:53 -0600

    Update script.js

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["~", "`", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ",", "'", "|", "/"];
+
+var generateBtn = document.querySelector("#generate");
